c6e0f601e7ecf018c68e44f7a88a9aa3e259f002
Lenka Vincenova <2371607v@student.gla.ac.uk>
2020-02-02 19:52:37 +0000

    chapter6-showing database results

--- rango/admin.py
+++ rango/admin.py
@@ -13,6 +13,10 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
     # inlines = [ChoiceInline]
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
 
-admin.site.register(Category)
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+
